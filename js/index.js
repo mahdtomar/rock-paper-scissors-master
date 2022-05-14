d9bb3cc992829bd3241a05b6ebbffeb01ea62ca5
Omar Mahdy <72965518+mahdtomar@users.noreply.github.com>
2022-05-14 19:20:50 +0200

    fixing styling

--- js/index.js
+++ js/index.js
@@ -3,11 +3,25 @@ const body = document.body;
 const div = document.createElement("div");
 const rulesImg = document.createElement("img");
 const container = document.querySelector(".container");
+const choices = document.querySelector(".choices");
+
 let rulesDiv = div.cloneNode(true);
+const blackScreen = div.cloneNode();
+
+blackScreen.setAttribute("class", "blackscreen");
 rulesDiv.appendChild(rulesImg);
 rulesDiv.setAttribute("class", "rulesdiv");
 rulesImg.setAttribute("src", "./images/image-rules.svg");
 
 rules.addEventListener("click", () => {
   container.appendChild(rulesDiv);
+  container.style.zIndex = "-1";
+  body.appendChild(blackScreen);
 });
+
+blackScreen.addEventListener("click", () => {
+  body.removeChild(blackScreen);
+  container.removeChild(rulesDiv);
+  container.style.zIndex = "1";
+});
+
